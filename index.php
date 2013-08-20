4d49d60b4a78738947595da53dac8c9bbb8a6cd4
psh24053 <psh24053@yahoo.cn>
2013-08-20 23:08:31 +0800

    11

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8">
+		<title>P Desktop</title>
+		<script type="text/javascript" src="pdesktop/jquery-1.10.2.min.js"></script>
+		<script type="text/javascript" src="pdesktop/pdesktop.js"></script>
+	</head>
+	
+	<body>
+	
+	</body>
+</html>
